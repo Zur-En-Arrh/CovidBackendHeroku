afde5491e3589d165569d3637b6aa053aff187b1
Matheus Zaché Gonçalves <matheuszache2@gmail.com>
2020-04-18 23:16:40 -0300

    Dinamico

--- src/index.js
+++ src/index.js
@@ -3,91 +3,88 @@ const app = express()
 const fs = require('fs')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const caminho = __dirname+'/../AnalisePopulacionalTratada.csv'
+const axios = require('axios')
+const geocodificacao = 'https://maps.googleapis.com/maps/api/geocode/json'
 
 app.use(express.static('.'))
 app.use(cors())
 app.use(bodyParser.json())
 
-function pegarLocais(results, analisePopulacional) {
-    const cidades = []
-    results.forEach(place => {
-        if(place) {
-            let cidade = {}
-            analisePopulacional.forEach(obj => {
-                let lugar = place.vicinity || ''
-                let result = lugar.indexOf(obj.nome) > -1
-                if(result) {
-                    cidade.nome = obj.nome
-                }else if(place.plus_code) {
-                    lugar = place.plus_code.compound_code
-                    result = lugar.indexOf(obj.nome) > -1
-                    if(result) {
-                        cidade.nome = obj.nome
-                    }
-                }
+
+async function ajax(lugar) {
+    try{
+        const resposta = await axios(geocodificacao, {
+            params: {
+                address: lugar.vicinity,
+                key: 'AIzaSyBofqU_V-dLDeMwfha5K8RcYwxwABYD_tY'
+            }
+        })
+        
+        let obj = {}
+        resposta.data.results.forEach(endereco => {
+            endereco.address_components.forEach(component => {
+                if(component.types[0] == 'administrative_area_level_2')
+                    obj.cidade = component.long_name
+                else if(component.types[0] == 'administrative_area_level_1')
+                    obj.estado = component.short_name
+                else if(component.types[0] == 'country')
+                    obj.pais = component.short_name
             })
+        })
 
-            cidade.cobertura = place.cobertura
-            cidade.custo = place.custo
-            
-            cidades.push(cidade)
-        }
-    })
-    console.log(cidades)
-    return cidades
-}
+        obj.custo = lugar.custo
+        obj.cobertura = lugar.cobertura
 
+        return obj
+    }catch(msg){
+        //console.log('Deu ruim', msg)
+        return {}
+    }
+    
+} 
 
-app.get('/', (req, res) => {res.send('Teste')})
-
-app.post('/construirJSON', (req, res, next) => {
-    //Tratamento do arquivo
+function consultarCSV(lugar) {
+    //console.log(lugar)
+    let estado = lugar.estado == 'BR' || lugar.estado == 'US'?'SP':lugar.estado
+    let separador = ','
+    const caminho = __dirname+`/../DemographicData/${estado}DemographicData.csv`
     const arquivo = fs.readFileSync(caminho, 'utf-8')
     const linhas = arquivo.split('\n')
     const analisePopulacional = linhas.map(linha => {
-        dados = linha.split(',')
-        return {id: dados[0], nome: dados[1]}
-    }).filter(elemento => elemento.id != 'Posição' && elemento.id != '')
+        dados = linha.split(separador)
+        return {id: dados[0], nome: dados[1], estado, cobertura: lugar.cobertura, custo: lugar.custo}
+    }).filter(elemento => elemento.id != 'Posição' && elemento.id != '' && elemento.id != 'Position')
+    return analisePopulacional.find(linha => linha.nome === lugar.cidade)
+}
 
+//app.get('/', (req, res) => {res.send('Teste')})
+
+app.post('/construirJSON', (req, res, next) => {
+    //Tratamento do arquivo
     //Tratar os hospitais/estados que recebi
-    const {lugares, filtro} = req.body
-    const locais = pegarLocais(lugares, analisePopulacional)
-    novosLocais = locais.filter(lugar => lugar.nome.trim() != "")
+    const {lugares} = req.body
 
-    let i = 0
-    console.log('Novas Cidades', novosLocais)
-    const cover = {value: []}
-    const city = {value: []}
-    const cost = {value: []}
-    novosLocais.forEach(local => {
-        let cidade = analisePopulacional.find(cidade => cidade.nome == local.nome.trim())
-        if(cidade == undefined){
-            console.log('Deu ruim', string)
-        } else {
-            cover.value.push(local.cobertura)
-            city.value.push(cidade.id)
-            cost.value.push(local.custo)
-        }
-        //return {id: i++, cidade: cidade.id, nome: cidade.nome, cobertura: local.cobertura, custo: local.custo}
+    const promises = lugares.map(lugar => ajax(lugar))
+
+    Promise.all(promises).then(resultado => {
+        const filtro = resultado.filter(local => local.pais === 'BR' && local !== {} && local !== undefined)
+        const mapeamento = filtro.map(lugar => consultarCSV(lugar))
+        //Montar o array de cidades, custo, ids, cobertura
+        const cover = {value: []}
+        const city = {value: []}
+        const cost = {value: []}
+        mapeamento.forEach(lugar => {
+            cover.value.push(lugar.cobertura)
+            city.value.push(`${lugar.estado}-${lugar.id}`)
+            cost.value.push(lugar.custo)
+        })
+
+        res.status(200).send(JSON.stringify({city, cover, cost}))
+        
     })
-
-    /*console.log('Novo CSV', novoCSV)
-
-    let stringFinal = ''
-
-    novoCSV.forEach(linha => {
-        stringFinal += `${linha.id},${linha.cidade},${linha.cobertura},${linha.custo}\n`
-    })*/
-
-    //fs.writeFile(__dirname + `/ocorrenciaCidades.csv`, stringFinal, err => res.status(500).send(err))
-    res.status(200).send(JSON.stringify({city, cover, cost}))
-    //console.log(novoCSV)
-    
-    //res.send((cidades))*/
 })
 
-
-app.listen(process.env.PORT || 3000, () => {
+//process.env.PORT || 3000
+app.listen(3000, () => {
     console.log('Aplicação rodando')
 })
