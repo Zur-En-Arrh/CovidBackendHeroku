4b72437db36b47dbc683e6fc721adabe34379101
Matheus Zaché Gonçalves <matheuszache2@gmail.com>
2020-05-18 16:38:08 -0300

    *SPA-Services*

--- src/index.js
+++ src/index.js
@@ -125,6 +125,6 @@ app.get('/data/:csv', (req, res) => {
 })
 
 //process.env.PORT || 3000
-app.listen(3000,() => {
+app.listen(process.env.PORT || 3000,() => {
     console.log('Aplicação rodando')
 })
