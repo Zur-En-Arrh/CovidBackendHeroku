7b947281777621f2cecd55aff94d3262b5e6aae9
Matheus Zaché Gonçalves <matheuszache2@gmail.com>
2020-05-18 16:36:04 -0300

    *SPA-Services*

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ async function ajax(lugar) {
     
 } 
 
-function consultarCSV(lugar) {
+function construirVetor(lugar) {
     //console.log(lugar)
     let estado = lugar.estado == 'BR' || lugar.estado == 'US'?'SP':lugar.estado
     let separador = ','
@@ -51,10 +51,21 @@ function consultarCSV(lugar) {
     const arquivo = fs.readFileSync(caminho, 'utf-8')
     const linhas = arquivo.split('\n')
     const analisePopulacional = linhas.map(linha => {
-        dados = linha.split(separador)
+        let dados = linha.split(separador)
         return {id: dados[0], nome: dados[1], estado, cobertura: lugar.cobertura, custo: lugar.custo}
     }).filter(elemento => elemento.id != 'Posição' && elemento.id != '' && elemento.id != 'Position')
     return analisePopulacional.find(linha => linha.nome === lugar.cidade)
+}
+
+function abrirCSV(nome) {
+    const arquivo = fs.readFileSync(__dirname+`/../CSV/${nome}.csv`, 'utf-8')
+    const linhas = arquivo.split('\n')
+    const resultado = []
+    linhas.forEach(linha => {
+        let colunas = linha.split(',')
+        resultado.push(colunas)
+    })
+    return resultado
 }
 
 //app.get('/', (req, res) => {res.send('Teste')})
@@ -68,23 +79,52 @@ app.post('/construirJSON', (req, res, next) => {
 
     Promise.all(promises).then(resultado => {
         const filtro = resultado.filter(local => local.pais === 'BR' && local !== {} && local !== undefined)
-        const mapeamento = filtro.map(lugar => consultarCSV(lugar))
+        const mapeamento = filtro.map(lugar => construirVetor(lugar))
         //Montar o array de cidades, custo, ids, cobertura
         const cover = {value: []}
         const city = {value: []}
         const cost = {value: []}
         mapeamento.forEach(lugar => {
-            cover.value.push(lugar.cobertura)
-            city.value.push(`${lugar.estado}-${lugar.id}`)
-            cost.value.push(lugar.custo)
+            if(lugar !== undefined) {
+                cover.value.push(lugar.cobertura)
+                city.value.push(`${lugar.estado}-${lugar.id}`)
+                cost.value.push(lugar.custo)
+            }
         })
 
+        //fs.writeFile(__dirname+'/cidades.json', , err => console.log(err))
+        console.log(JSON.stringify({city, cover, cost}))
         res.status(200).send(JSON.stringify({city, cover, cost}))
         
     })
 })
 
+app.get('/data/:csv', (req, res) => {
+    const nome = req.params.csv
+    const resultado = abrirCSV(nome)
+    //console.log('Cidades', resultado)
+    if(nome == 'Cities') {
+        const dados = []
+        resultado.filter(vetor => vetor[0] !== 'State').forEach(vetor => {
+            vetor.map(celula => {
+                if(celula !== 'False' && celula.length > 2)
+                    dados.push(celula)
+            })
+        })
+
+        res.status(200).json(dados)
+    }else if (nome == 'States') {
+        const siglas = []
+        resultado.map(vetor => {
+            if(vetor[0] !== '')
+                siglas.push(vetor[1])
+        })
+
+        res.status(200).json(siglas)
+    }
+})
+
 //process.env.PORT || 3000
-app.listen(process.env.PORT || 3000,() => {
+app.listen(3000,() => {
     console.log('Aplicação rodando')
 })
